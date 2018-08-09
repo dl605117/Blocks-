6007e7c8afb8c9265f02f00c0fdc468cf2a4f5d1
Darren <dl605117@gmail.com>
2018-08-08 22:02:40 -0700

    Added Vec2

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -42,5 +42,4 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
-	//k
 }
---
+++ Engine/Vec2.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <algorithm>
+
+template <typename T>
+class Vec2
+{
+public:
+	Vec2() = default;
+	Vec2( T x,T y )
+		:
+		x( x ),
+		y( y )
+	{
+	}
+	Vec2 operator+( const Vec2& rhs ) const
+	{
+		return Vec2( rhs.x + x,rhs.y + rhs.y );
+	}
+	Vec2& operator+=( const Vec2& rhs )
+	{
+		return *this + rhs;
+	}
+	Vec2 operator-( const Vec2& rhs ) const
+	{
+		return Vec2( x - rhs.x,y - rhs.y );
+	}
+	Vec2& operator-=( const Vec2& rhs )
+	{
+		return *this - rhs;
+	}
+	Vec2 operator*( T scalar ) const
+	{
+		return Vec2( x * scalar,y * scalar );
+	}
+	Vec2& operator*=( T scalar )
+	{
+		return *this * scalar;
+	}
+	T GetLengthSq() const
+	{
+		return x * x + y * y;
+	}
+	T GetLength() const
+	{
+		return std::sqrt( GetLengthSq() );
+	}
+	Vec2& Normalize()
+	{
+		return *this->GetNormalized();
+	}
+	Vec2 GetNormalized() const
+	{
+		const T length = GetLength();
+		return Vec2( x / length,y / length );
+	}
+private:
+	T x;
+	T y;
+};
