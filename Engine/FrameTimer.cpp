439351892f407fffc1eda4cdd93fb5bebb6b7572
Darren <dl605117@gmail.com>
2018-08-09 22:35:42 -0700

    Added FrameTimer

---
+++ Engine/FrameTimer.cpp
@@ -0,0 +1,16 @@
+#include "FrameTimer.h"
+
+using namespace std::chrono;
+
+FrameTimer::FrameTimer()
+{
+	last = steady_clock::now();
+}
+
+float FrameTimer::Mark()
+{
+	const auto old = last;
+	last = steady_clock::now();
+	const duration<float> time = last - old;
+	return time.count();
+}
